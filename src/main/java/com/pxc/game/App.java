cd6e36b4c1a008813efd9866745e36d3bd73b98a
pengxincheng <871666430@qq.com>
2022-11-30 10:24:24 +0800

    feat: 将配置文件抽离出来

--- src/main/java/com/pxc/game/App.java
+++ src/main/java/com/pxc/game/App.java
@@ -4,6 +4,8 @@ import com.pxc.game.script.Script;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  */
@@ -15,7 +17,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         logger.info("5秒后开始运行");
-        // TimeUnit.SECONDS.sleep(5);
+         TimeUnit.SECONDS.sleep(5);
 
 
          Script.zhuaGui();
